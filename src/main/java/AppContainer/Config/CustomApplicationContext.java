3ae2462620398e4bfe3cc1949847cca21d9e0f10
Petre Vane <petre@MacBook-Air.local>
2021-04-15 20:44:08 +0200

    Added necessary configurations

--- src/main/java/AppContainer/Config/CustomApplicationContext.java
+++ src/main/java/AppContainer/Config/CustomApplicationContext.java
@@ -1,4 +1,4 @@
-package AppContainer;
+package AppContainer.Config;
 
 
 import org.springframework.context.annotation.Bean;
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan("AppContainer")
-public class ApplicationContext {
+public class CustomApplicationContext {
 
     @Bean
     public ViewResolver viewResolver() {
---
+++ src/main/java/AppContainer/Config/DispatcherServletInitializer.java
@@ -0,0 +1,22 @@
+package AppContainer.Config;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { CustomApplicationContext.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+}

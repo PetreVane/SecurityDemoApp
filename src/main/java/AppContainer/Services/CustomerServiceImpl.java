f643efb32bd81850a28b5138527d5262811ac713
Petre Vane <PetreVane@users.noreply.github.com>
2021-04-22 20:03:20 +0200

    Added: - hibernate, mysql and spring-orm dependencies - dataSource, sessionFactory and TransactionManagement beans

---
+++ src/main/java/AppContainer/Services/CustomerServiceImpl.java
@@ -0,0 +1,46 @@
+package AppContainer.Services;
+
+import AppContainer.DAO.CustomerDAO;
+import AppContainer.Models.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    private CustomerDAO customerDAO;
+
+    @Override
+    @Transactional
+    public List<Customer> getCustomers() {
+        return customerDAO.getCustomers();
+    }
+
+    @Override
+    @Transactional
+    public void saveCustomer(Customer theCustomer) {
+        customerDAO.saveCustomer(theCustomer);
+    }
+
+    @Override
+    @Transactional
+    public Customer getCustomer(int theId) {
+        return customerDAO.getCustomer(theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteCustomer(int theId) {
+        customerDAO.deleteCustomer(theId);
+    }
+
+    @Override
+    @Transactional
+    public List<Customer> searchCustomer(String customerName) {
+        return customerDAO.searchCustomer(customerName);
+    }
+}

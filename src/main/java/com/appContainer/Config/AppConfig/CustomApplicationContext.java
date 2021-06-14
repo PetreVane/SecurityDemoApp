0ae8a8739a44424fe72bfaf053fa2d54ac640766
Petre Vane <>
2021-06-14 21:47:15 +0200

    Loaded connection properties from environment

--- src/main/java/com/appContainer/Config/AppConfig/CustomApplicationContext.java
+++ src/main/java/com/appContainer/Config/AppConfig/CustomApplicationContext.java
@@ -1,10 +1,13 @@
 package com.appContainer.Config.AppConfig;
 
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
@@ -13,16 +16,47 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 @Configuration
 @EnableWebMvc
 @EnableTransactionManagement
 @ComponentScan("com.appContainer")
+@PropertySource({"classpath:persistence-mysql.properties" })
 public class CustomApplicationContext implements WebMvcConfigurer {
+
+    @Autowired
+    private Environment environment;
+    private final Logger logger = Logger.getLogger(getClass().getName());
+
+
+    @Bean
+    public DataSource getDatasource() {
+
+        logger.info("Logging in with jdbc.url=" + environment.getProperty("jdbc.url"));
+        logger.info("Logging in with jdbc.user=" + environment.getProperty("jdbc.user"));
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("jdbc.driver")));
+        dataSource.setUrl(environment.getProperty("jdbc.url"));
+        dataSource.setUsername(environment.getProperty("jdbc.user"));
+        dataSource.setPassword(environment.getProperty("jdbc.password"));
+        return dataSource;
+    }
+
+    //helper which reads environment properties and casts them as int
+    private int getIntProperty(String propertyName) {
+        String property = environment.getProperty(propertyName);
+        return Integer.parseInt(Objects.requireNonNull(property));
+    }
+
 
     @Bean
     public InternalResourceViewResolver viewResolver() {
@@ -41,51 +75,30 @@ public class CustomApplicationContext implements WebMvcConfigurer {
     }
 
 
-    // Hibernate config
-    @Bean
-    public DataSource getDatasource() {
-
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&amp");
-
-        // I know these will be public -- this is intentional
-        dataSource.setUsername("springstudent");
-        dataSource.setPassword("springstudent");
-
-        return dataSource;
-    }
-
     @Bean
     public SessionFactory getSessionFactory() throws IOException {
 
-        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
-        sessionFactoryBean.setPackagesToScan("com/appContainer");
-
-        //getHibernateProperties method is a private method
-        sessionFactoryBean.setHibernateProperties(getHibernateProperties());
-        sessionFactoryBean.setDataSource(getDatasource());
-        sessionFactoryBean.afterPropertiesSet();
-
-        return sessionFactoryBean.getObject();
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setPackagesToScan(environment.getProperty("hibernate.packagesToScan"));
+        sessionFactory.setHibernateProperties(getHibernateProperties());
+        sessionFactory.setDataSource(getDatasource());
+        sessionFactory.afterPropertiesSet();
+        return sessionFactory.getObject();
     }
 
 
-    private static Properties getHibernateProperties() {
-
+    private Properties getHibernateProperties() {
         Properties hibernateProperties = new Properties();
-        hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
-        hibernateProperties.put("hibernate.show_sql", true);
-        // other properties
-
+        hibernateProperties.put("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+        hibernateProperties.put("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
         return hibernateProperties;
     }
 
     @Bean
+    @Autowired
     public HibernateTransactionManager getTransactionManager() throws IOException{
         HibernateTransactionManager transactionManager = new HibernateTransactionManager();
         transactionManager.setSessionFactory(getSessionFactory());
-
         return transactionManager;
     }
 
